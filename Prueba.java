51e0c4c5eb05bc8069a9459d1eb49b4f48473df9
Oscarcg02 <oscarcg02@gmail.com>
2020-10-07 09:34:52 +0200

    mi nombre otra vez

--- Prueba.java
+++ Prueba.java
@@ -5,7 +5,8 @@ public class Prueba {
 		System.out.println("Oscar Campoamor Getino");
 		System.out.println("Pobladura del Bernesga 2002");
 		System.out.println("Pobladura del Bernesga Windows 10");
-		
+		System.out.println("Oscar Campoamor Getino");
+
 	}
 
 }
